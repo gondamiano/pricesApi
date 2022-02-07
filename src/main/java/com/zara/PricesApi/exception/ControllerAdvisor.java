965c7cbf9b58c7c3735f6c648074cd1162e07bf6
Gonzalo Damiano <gondamiano@gmail.com>
2022-02-07 22:32:14 +0100

    delete unused import

--- src/main/java/com/zara/PricesApi/exception/ControllerAdvisor.java
+++ src/main/java/com/zara/PricesApi/exception/ControllerAdvisor.java
@@ -1,11 +1,9 @@
 package com.zara.PricesApi.exception;
 
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
